81ea1926c82e64ae6d0a18badcef8c7c5c7dd070
moh223322 <moh223322@yahoo.com>
2017-08-17 19:31:06 +0200

    modification for heroku

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require("express");
 
 const app = express();
 var bodyParser = require('body-parser');
-
+const port = process.env.PORT || 3000 
 app.use(bodyParser.json());
 app.use(express.static(__dirname+"/public"));
 
 app.get("/:language",(req,res)=>{
 	res.send(`your language is ${req.params.language}` )
 })
-app.listen(3000,()=>{
-	console.log("server started at port 3000");
+
+
+app.listen(port,()=>{
+	console.log(`server started at port ${port}`);
 });
